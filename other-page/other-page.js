b042efee0063191aab86cff7b02bad10ac5f59c0
Benatovich <sambenatovich@gmail.com>
2022-01-07 19:37:08 -0500

    I think event listeners are all set. moving on to functions next to confirm

--- other-page/other-page.js
+++ other-page/other-page.js
@@ -2,8 +2,69 @@ import { checkAuth, logout } from '../fetch-utils.js';
 
 checkAuth();
 
-const logoutButton = document.getElementById('logout');
+const listEl = document.querySelector('.list');
+const itemForm = document.querySelector('.item-form');
+const logoutButton = document.querySelector('#logout');
+const deleteButton = document.querySelector('.delete-button');
+
+itemForm.addEventListener('submit', async(e) => {
+    // on submit, create a item, reset the form, and display the items
+    e.preventDefault();
+
+    const data = new FormData(itemForm);
+
+    const item = data.get('item');
+    const quantity = data.get('quantity');
+
+    await createItem(item, quantity);
+    // reset form
+    itemForm.reset();
+    //fetch and display items
+    await fetchAndDisplayItems();
+});
+
+async function fetchAndDisplayItems() {
+    // fetch the items
+    const items = await getItems();
+    // clear out itemsEl
+    listEl.textContent = '';
+    // display the list of items
+    for (let item of items) {
+        const itemEl = document.createElement('p');
+        itemEl.classList.add('item');
+        itemEl.textContent = `${item.quantity} ${item.item}`;
+        
+        // be sure to give each item an event listener
+        // on click, complete that item
+        if (item.bought === true) {
+            itemEl.classList.add('bought');
+        } else {
+        
+            itemEl.classList.add('not-bought');
+            itemEl.addEventListener('click', async() => {
+                await buyItem(item.id);
+                fetchAndDisplayItems();
+            });
+        }
+
+        itemsEl.append(itemEl);
+    }
+
+}
+
+// add an on load listener that fetches and displays items on load
+window.addEventListener('load', async() => {
+    await fetchAndDisplayItems();
+});
 
 logoutButton.addEventListener('click', () => {
     logout();
+});
+
+
+deleteButton.addEventListener('click', async() => {
+    // delete all items
+    await deleteAllItems();
+    // then refetch and display the updated list of items
+    await fetchAndDisplayItems();
 });
